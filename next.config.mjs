95fb1a8dfde4690e2702ad3b5ec90db515a1cd8e
Fahad Khan <newpwafk15520041@gmail.com>
2025-08-16 07:41:27 +0000

    Hamari app me kuch badlav hone hai 1 ise pwa banana hai zyada pages cach

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own cache strategy to the bottom of the page
+});
+
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.ibb.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-            }
-        ],
-    },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      }
+    ],
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
