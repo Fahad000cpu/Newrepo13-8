a5ccd93e7613764668628e98c4d3391591aefab5
Fahad Khan <newpwafk15520041@gmail.com>
2025-08-16 07:45:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      }
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
